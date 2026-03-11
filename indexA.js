015a2fce61db6b31737bdacc8a021142ad6a39f4
konstantinvilkovskis-crypto <konstantin.vilkovskis@gmail.com>
2026-03-11 20:00:42 +0200

    Add files via upload

--- indexA.js
+++ indexA.js
@@ -6,7 +6,7 @@ buttonLogin.addEventListener('click', function() {
     let value = input.value 
 
     if (value === 'Teacher') {
-        window.location.href = "https://konstantinvilkovskis-crypto.github.io/E-diktant---student/"
+        window.location.href = "file:///C:/Users/User/Documents/Projects/e-diktant(teacher)/indexB.html"
     } else {
         window.location.href = "https://konstantinvilkovskis-crypto.github.io/E-diktant---student/"
     }
