64fae835c6367fa92fccdfc09b76aed1437399b4
konstantinvilkovskis-crypto <konstantin.vilkovskis@gmail.com>
2026-03-11 20:42:09 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+let buttonLogin = document.querySelector('.confirm')
+let input = document.querySelector('.input')
+
+buttonLogin.addEventListener('click', function() {
+   
+    let value = input.value 
+
+    if (value === 'Teacher') {
+        window.location.href = ""
+    } else {
+        window.location.href = ""
+    }
+})
